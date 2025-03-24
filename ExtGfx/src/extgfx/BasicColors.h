6d4698b603181785c13a6063aa3db1e851282544
PB <brouzda@datasys.cz>
2025-03-24 14:12:25 +0100

    lepsi version info

--- ExtGfx/src/extgfx/BasicColors.h
+++ ExtGfx/src/extgfx/BasicColors.h
@@ -5,6 +5,7 @@
 #define	EG_BLACK   0x0000
 #define	EG_BLUE    0x001F
 #define	EG_RED     0xF800
+#define	EG_LIGHT_RED     0xFAEB
 #define	EG_GREEN   0x07E0
 #define EG_CYAN    0x07FF
 #define EG_MAGENTA 0xF81F
--- ExtGfx/src/extgfx/ExtGfx.h
+++ ExtGfx/src/extgfx/ExtGfx.h
@@ -3,7 +3,7 @@
 
 #include <Arduino.h>
 
-#define EXTGFX_CORE_VERSION "2.0.1"
+#define EXTGFX_CORE_VERSION "2.0.2"
 
 /**
  * Zajistuje kompatibilitu mezi Adafruit_GFX a Arduino_GFX displeji.
@@ -13,12 +13,12 @@
     // Arduino_GFX mode
     #include <Arduino_GFX_Library.h>
     #define EXTGFX_DISPLAY_TYPE Arduino_GFX
-    #define EXTGFX_VERSION EXTGFX_CORE_VERSION "-Arduino_GFX"
+    #define EXTGFX_VERSION "ExtGfx-" EXTGFX_CORE_VERSION "-Arduino_GFX"
 #else
     // Adafruit_GFX mode
     #include <Adafruit_GFX.h>
     #define EXTGFX_DISPLAY_TYPE Adafruit_GFX
-    #define EXTGFX_VERSION EXTGFX_CORE_VERSION "-Adafruit_GFX"
+    #define EXTGFX_VERSION "ExtGfx-" EXTGFX_CORE_VERSION "-Adafruit_GFX"
 #endif
 
 #endif
